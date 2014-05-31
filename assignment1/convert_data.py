7b3b99c1f9882684539d6c9a3a77ff6e70cc4dcd
Dean Kleissas <dean.kleissas@gmail.com>
2014-05-31 16:31:21 -0400

    simplified assignment 1

---
+++ assignment1/convert_data.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import math
+
+# Fill in the missing code! Sections labeled TODO need to be completed
+# before the program will run
+def latlong2deg(lat,lon):
+	# This function should take latitude and longitude in a radians and print out degrees
+	# example: latlong2deg(0.678081742,-1.34456918)
+	# output: 38.851222,-77.038139
+
+	# TODO: Call rad2deg to convert from radians to degrees
+	lat_degrees = 
+	long_degrees = 
+		
+	# TODO: Complete the line below to print result to the screan
+	# Remember how to put float numbers (numbers with decimal values)
+	# into a string?
+	# Format should be: lat,lon  ex: 38.851222,-77.038139
+	print "" % ()
+
+
+def rad2deg(rad):
+	# TODO: compute the degree value from the radian value
+	degrees = 
+
+	# Return the value!
+	return degrees
+
+
+#### Start of the program ####
+
+# TODO: Call latlong2deg() for each point you want to convert
+latlong2deg(XXX,XXXX) # This is an example of how you would call it (replace XXX with real data)
+
+
